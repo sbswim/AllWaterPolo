f4c2ab2d828ca4c2f19e287647aa1829d5391ae8
sbswim <seth.berke@Seths-Laptop.local>
2016-04-13 14:54:14 -0400

    Added Page Dividers
    Changed html back to php; added page dividers and their respective
    styles

--- index.php
+++ index.php
@@ -5,5 +5,20 @@
 </head>
 <body>
 <p> The Start of something New </p>
+
+<div class="header">
+   <h1 class="Title"> AllWaterPolo </h1>
+</div>
+
+<div class="navbar">
+
+</div>
+<div class="content">
+
+</div>
+<div class="footer">
+
+</div>
+
 </body>
 </html>
