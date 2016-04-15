201a93dfa12ec264d69516daea62b939fe718f43
sbswim <seth.berke@Seths-Laptop.attlocal.net>
2016-04-15 17:06:26 -0400

    Added js file, made additions to styles and index
    started to construct navbar

--- index.php
+++ index.php
@@ -3,6 +3,12 @@
 <head>
    <link rel="stylesheet" styles.css type="text/css" href="styles.css">
    <link rel="stylesheet" styles.css type="text/css" href="normalize.css">
+   <script type="text/javascript" src="script.js"></script>
+
+   <meta charset="utf-8">
+   <meta name="description" content="All Water Polo Information" >
+   <meta name="keywords" content="WaterPolo,waterpolo,news,information">
+   <meta name="author" content="Seth Berke">
 </head>
 <body>
 
@@ -12,7 +18,12 @@
 </div>
 
 <div class="navbar">
-
+<ul>
+   <li> <a href="#">Page1</li>
+   <li> <a href="#">Page2</li>
+   <li> <a href="#">Page3</li>
+   <li> <a href="#">Page4</li>
+</ul>
 </div>
 <div class="content">
 
