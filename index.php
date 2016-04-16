2cfc21301fe6978617741568fa8820f7a9753925
sbswim <seth.berke@Seths-Laptop.attlocal.net>
2016-04-16 15:32:53 -0400

    Nav redone, images added, CSS changes
    Completely redid navigation bar
    Changed name to the inside game
    Added all images
    Modified Footer
    Made CSS changes respectively

--- index.php
+++ index.php
@@ -4,32 +4,56 @@
    <link rel="stylesheet" styles.css type="text/css" href="css/styles.css">
    <link rel="stylesheet" styles.css type="text/css" href="css/normalize.css">
    <script type="text/javascript" src="script.js"></script>
-   <title> AllWaterPolo </title>
+   <title> The Inside Game </title>
+   <link rel="icon"  type="image/png" href="assets/img/favicon.ico" />
 
    <meta charset="utf-8">
    <meta name="description" content="All Water Polo Information" >
-   <meta name="keywords" content="WaterPolo,waterpolo,news,information">
+   <meta name="keywords" content="WaterPolo,waterpolo,information,attitude">
    <meta name="author" content="Seth Berke">
 </head>
 <body>
 
 <div class="wrapper">
 <div class="header">
-   <h1 class="title"> AllWaterPolo </h1>
+   <a href="index.php">
+   <h1 class="title"> The Inside Game </h1>
+   <h2 class="subtitle"> Simply Dedicated to Water Polo</h2>
+   <img class="wpball"src="assets/img/WPBall.png" />
 </div>
 
 <div class="navbar">
 <ul>
-   <li> <a href="#">Page1</li>
-   <li> <a href="#">Page2</li>
-   <li> <a href="#">Page3</li>
-   <li> <a href="#">Page4</li>
+   <li> <a href="#">Get Started!</li>
+   <li> <a href="#">Attitude</li>
+   <li> <a href="#"> Training </li>
+      <li> <a href="#"> Injury Prevention </li>
+      <li> <a href="#">Information
+         <ul>
+            <li> <a href="#">Rules </li>
+            <li> <a href="#">Positions </li>
+            <li> <a href="#">Plays </li>
+            <li> <a href="#">Tournments </li>
+         </ul>
+         </li>
+   <li> <a href="#"> About Us </li>
 </ul>
 </div>
-<div class="content">
 
+<div class="content">
+<h1 class="ourgoal"> Our "Goal" </h1>
 </div>
+
 <div class="footer">
+    <p>Contact Us! <br> @allwaterpolo</p>
+    <a href="#"><img src ="assets/img/Facebook_Png_01.png" class="facebook" id="FaceBook Link" /> </a>
+    <a href="#"><img src ="assets/img/Uiconstock-Socialmedia-Instagram.ico" class="instagram" id="Instagram Link" /> </a>
+     <a href="#"><img src ="assets/img/Twitter_Logo_Hd_Png_03.png" class="twitter" id="Twitter Link" /> </a>
+    <a href="#"><img src ="assets/img/Youtube_icon.png" class="youtube" id="YouTube Link" /> </a>
+    <div class= copyright>
+    Copyright &copy; 2016<script>new Date().getFullYear()>2016&&document.write("-"+new Date().getFullYear());</script>, <br> All Water Polo. All rights reserved
+    </div>
+
 
 </div>
 </div>
